6fe63cf2368b69e13c4c9f9a6157e677d36cb02c
davidcas34 <davidcas34@users.noreply.github.com>
2020-02-10 16:11:27 -0500

    AddedGame
    Actual Unity Game

---
+++ GameDesign/Assets/Goal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    CapsuleCollider CC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CC = gameObject.GetComponent<CapsuleCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Goal"))
+        {
+            int index = SceneManager.GetActiveScene().buildIndex;
+            if(index == SceneManager.sceneCountInBuildSettings - 2)
+            {
+                Debug.Log(SceneManager.sceneCount - 1);
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            SceneManager.LoadScene(index + 1);
+        }
+    }
+}
---
+++ GameDesign/Assets/SceneManger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManger : MonoBehaviour
+{
+    public void LoadScene(int num)
+    {
+        SceneManager.LoadScene(num);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+    }
+    public void loadNextScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
