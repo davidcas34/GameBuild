b7f85ef0b8040e672302c159d72157e1baf78a8f
davidcas34 <davidcas34@users.noreply.github.com>
2020-03-29 15:54:18 -0400

    Knight Model
    Added Knight
    Added Animation time
    Added transitions

---
+++ SisterRescue/Assets/SwordScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour
+{
+
+    Animator sword;
+    bool attacked = false;
+    float time;
+
+    void Start()
+    {
+        sword = gameObject.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if (!attacked)
+            {
+                sword.enabled = true;
+                sword.Play("Attack", 0, 0.50f);
+                attacked = true;
+                time = Time.time;
+            }
+            else
+            {
+                Debug.Log(Time.time);
+                Debug.Log(time);
+                if (Time.time - time > 0.1f)
+                {
+                    Debug.Log("Sub" + (Time.time - time).ToString());
+                    sword.enabled = false;
+                    attacked = false;
+                }
+            }
+        }
+    }
+}
